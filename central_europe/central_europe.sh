861ec1c1fbec423fd0021bcbdd8b8097eb9de85e
John Cairns <john@2ad.com>
2020-10-03 20:56:00 -0500

    roughly central europe and inset

--- central_europe/central_europe.sh
+++ central_europe/central_europe.sh
@@ -2,14 +2,14 @@
 
 # Regional Map: Route of Peter the Hermit’s Peasants’ Crusade, 1096 (see Map from Konstam, pages 48-49/ generate Europe, Upper Mediterranean and Western Anatolia Map Shell, perhaps 55 degrees North latitude to 30 degrees south, henceforth called “Europe-Mediterranean Map Shell”)
 
-WEST=20
-EAST=50
-SOUTH=30
-NORTH=55
+WEST=-5
+EAST=15
+SOUTH=43
+NORTH=48
 
 WIDTH=15c
 
-PROJECTION=-JM35/${WIDTH}
+PROJECTION=-JM5/${WIDTH}
 
 OPT="-V --FONT_ANNOT_PRIMARY=10p"
 
@@ -31,9 +31,6 @@ then
     exit 1
 fi
 
-# how to rose
-# city and poi points
-
 gmt set PS_LINE_CAP=ROUND PS_LINE_JOIN=ROUND PS_SCALE_X=1 PS_SCALE_Y=1 MAP_ORIGIN_X=1c MAP_ORIGIN_Y=1c
 
 BASEMAP='-B10dg10d -B+gwhite'
@@ -53,21 +50,28 @@ gmt begin central_europe
     # water bounds
     gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -N3/0.25p,220/220/220@${TRANS}
 
-    echo 10.3333333 36.8666667 14 0 4 LM Carthage | gmt text -F+f11p,Helvetica-Bold+jCB -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
-    cat cities.dat | gmt text -F+f11p,Helvetica-Bold+jCB -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
-gmt end
+    #-F+f11p,Helvetica-Bold+jCB
+    cat caps.dat | gmt text -F+f10p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    cat cities.dat | gmt text -F+f8p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
 
+gmt end
 # try gmt end show
 
-#gmt pscoast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${LINE} -G${LAND}/${LAND}/${LAND} ${PROJECTION} ${OPT} -Na/${PEN} -Ia/${PEN} ${BASEMAP} ${ROSE} > central_europe.ps
+IRECT='-R-10/30/35/55'
+IPROJ='-JM10/2i'
 
-#gmt psbasemap -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} ${BASEMAP} > basemap.ps
-#gmt pscoast -R${WEST}/${EAST}/${SOUTH}/${NORTH} -G${LAND}/${LAND}/${LAND} ${PROJECTION} ${OPT} > landmass.ps
-#gmt pscoast -R${WEST}/${EAST}/${SOUTH}/${NORTH} -S${LAKE}/${LAKE}/${LAKE} ${PROJECTION} ${OPT} > lakemass.ps
-#gmt pscoast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${LINE} ${PROJECTION} ${OPT} > coastline.ps
-#gmt pscoast -R${WEST}/${EAST}/${SOUTH}/${NORTH} -Ia/${PEN} ${PROJECTION} ${OPT}  > river.ps
-#gmt pscoast -R${WEST}/${EAST}/${SOUTH}/${NORTH} -Na/${PEN} ${PROJECTION} ${OPT} > national.ps
+gmt begin inset
+  gmt basemap ${IRECT} ${IPROJ} ${OPT} -B+ggrey
+  gmt coast ${IRECT}  ${IPROJ} ${OPT} -G${LAND}/${LAND}/${LAND}@${TRANS}
+  gmt coast ${IRECT} ${IPROJ} ${OPT} -S${LAKE}/${LAKE}/${LAKE}
+  gmt coast ${IRECT} ${IPROJ} ${OPT} -N3/0.25p,220/220/220@${TRANS}
 
-
-#gmt grd2cpt ${ETOPO1} -Cgrey > project.cpt
-#gmt grdimage ${ETOPO1} -n+c -I+a45+nt1 ${PROJECTION} -R${WEST}/${EAST}/${SOUTH}/${NORTH} -Cproject.cpt ${OPT} -K > topo.ps
+  gmt plot -W0.5p ${IRECT} ${IPROJ} ${OPT} <<EOF
+-5 43
+15 43
+15 48
+-5 48
+-5 43
+EOF
+    
+gmt end
