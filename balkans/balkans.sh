d29435a3e7b1ef0515d1c9fc3080128319d08a0b
John Cairns <john@2ad.com>
2021-01-05 12:19:51 -0600

    balkans shell

---
+++ balkans/balkans.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+WEST=5
+EAST=40
+SOUTH=34
+NORTH=47
+
+WIDTH=15c
+
+PROJECTION=-JM10/${WIDTH}
+
+OPT="-V --FONT_ANNOT_PRIMARY=10p"
+
+PEN=0.25p,200/200/200
+LINE=-W${PEN}
+LAND=255
+LAKE=170
+TRANS=15
+SCALEBAR="f35/40/40/500M"
+
+gmt gtd2cpt --show-sharedir
+
+# ETOPO1_Bed_g_gmt4.grd is the NETCDF encoded ETOPO1 dataset downloaded for GMT4 Bedrock
+
+ETOPO1=../ETOPO1_Bed_g_gmt4.grd
+
+if [ ! -f ${ETOPO1} ]
+then
+    echo "ETOPO1 GRID file is required"
+    exit 1
+fi
+
+gmt set PS_LINE_CAP=ROUND PS_LINE_JOIN=ROUND PS_SCALE_X=1 PS_SCALE_Y=1 MAP_ORIGIN_X=1c MAP_ORIGIN_Y=1c
+
+BASEMAP='-B10dg10d -B+gwhite'
+
+gmt begin balkans
+
+    gmt basemap -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} ${BASEMAP} 
+    gmt makecpt -Cgrey -T-10/2000
+    gmt grdimage ${ETOPO1} -n+c -I+a45+nt1 ${PROJECTION} -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${OPT}
+
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH}  ${PROJECTION} ${OPT} -G${LAND}/${LAND}/${LAND}@${TRANS}
+    
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -S${LAKE}/${LAKE}/${LAKE} 
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} ${LINE} 
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -Ia/0.5p,${LAKE}/${LAKE}/${LAKE}
+    # national bounds
+    #    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -N1/1.25p,225/225/225
+    # water bounds
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -L${SCALEBAR} -N3/0.25p,${LAKE}/${LAKE}/${LAKE}
+
+    #-F+f11p,Helvetica-Bold+jCB
+    #-F+f10p,Palatino-Roman+jCB    
+    # cat caps.dat | gmt text -F+f10p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    if [ -f city.dat ]
+    then
+        cat city.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f10p,Palatino-Roman+jCB    
+        cat city.dat | gmt plot -Sc2p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    fi
+
+    if [ -f battle.dat ]
+    then
+    
+        cat battle.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f10p,Palatino-Roman+jCB    
+        cat battle.dat | gmt plot -S+4p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    fi
+
+    if [ -f place.dat ]
+    then
+        cat place.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f6p,Palatino-Roman+jCB
+    fi
+
+
+gmt end
+# try gmt end show
+
